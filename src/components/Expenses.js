f16301b1aa7e98667a2357685dad422143d1803d
Ajayprajapati786 <prajapatisani9@gmail.com>
2023-06-01 17:56:02 +0530

    some styling

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -1,27 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
 
 const Expenses = () => {
-  const income = useSelector((state) => state.authentication.valuee);
+  const [filter, setFilter] = useState('All');
+  const transactions = useSelector((state) => state.authentication.valuee);
 
+  const handleFilterChange = (event) => {
+    setFilter(event.target.value);
+  };
 
-  console.log(income);
+  const filteredTransactions = filter === 'All' ? transactions : transactions.filter((transaction) => {
+    if (filter === 'Debit') {
+      return transaction.type === 'expense';
+    }
+    if (filter === 'Credit') {
+      return transaction.type === 'income';
+    }
+    return false;
+  });
+
   return (
-    <div>
-      Expenses:
-      {income.map((incomeItem) => (
+    <div className='container'>
+      <h1>Expenses:</h1>
+      <div>
+        <label className='mx-2'>
+          <input type='radio' value='All' checked={filter === 'All'} onChange={handleFilterChange} />
+          All
+        </label>
+        <label className='mx-2'>
+          <input type='radio' value='Debit' checked={filter === 'Debit'} onChange={handleFilterChange} />
+          Debit
+        </label>
+        <label className='mx-2'>
+          <input type='radio' value='Credit' checked={filter === 'Credit'} onChange={handleFilterChange} />
+          Credit
+        </label>
+      </div>
+      {filteredTransactions.map((transaction) => (
         <div
-          key={incomeItem.id}
+          key={transaction.id}
           style={{
-            borderRight: `5px solid ${incomeItem.type === 'income' ? 'green' : 'red'}`,
+            borderRight: `5px solid ${transaction.type === 'income' ? 'green' : 'red'}`,
             padding: '10px',
             marginBottom: '10px',
           }}
         >
-          <p>Text: {incomeItem.text}</p>
-          <p>Amount: {incomeItem.amount}</p>
-          <p>Date: {incomeItem.date}</p>
-          <p>Type: {incomeItem.type}</p>
+          <p>Text: {transaction.text}</p>
+          <p>Amount: {transaction.amount}</p>
+          <p>Date: {transaction.date}</p>
+          {/* <p>Type: {transaction.type}</p> */}
         </div>
       ))}
     </div>
